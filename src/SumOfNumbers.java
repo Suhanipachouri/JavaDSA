4ad7335e4774068f2aa05d62d775ae44e04da9a8
Suhanipachouri <suhani.pachouri_cs20@gla.ac.in>
2022-03-31 14:29:39 +0530

    created SumOfNumbers.java File.

---
+++ src/SumOfNumbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int res = sc.nextInt();
+        res += sc.nextInt();
+        System.out.println(res);
+    }
+}
